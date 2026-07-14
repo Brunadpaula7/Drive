82258989077d3e8d24b0a0c5199370cf991d7ca6
Brunadpaula7 <119906774+Brunadpaula7@users.noreply.github.com>
2026-07-14 14:10:21 -0300

    Atualizar o server.ts

--- server.ts
+++ server.ts
@@ -49,8 +49,8 @@ async function startServer() {
   } else {
     const distPath = path.join(process.cwd(), 'dist');
     app.use(express.static(distPath));
-    app.get('*', (req, res) => {
-      res.sendFile(path.join(distPath, 'index.html'));
+    app.get('/*splat', (req, res) => {
+  res.sendFile(path.join(distPath, 'index.html'));
     });
   }
 
